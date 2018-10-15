f49ded7a8a49650c223121f1ac0e5fc1083da254
Norberto Oliveira Junior <norbejunior@gmail.com>
2018-10-14 23:48:08 -0300

    Implements Slugy functions for Phoenix applications
    * Implemented Slugy.slugify/2 a function to use
      on the changeset pipeline of a Phoenix schema module
      to set the slug into the slug field

--- lib/slugy.ex
+++ lib/slugy.ex
@@ -1,13 +1,61 @@
 defmodule Slugy do
+  @moduledoc ~S"""
+  Slugy is a Phoenix library to generate slugs to ecto schema fields
+
+  Let's suppose we have a Post schema and we want to generate a slug from `title` field
+  and save it to the `slug` field. To achieve that we need to call Slugy.slugify/2
+  following the changeset pipeline passing the desireable field. Slugy.slugify/2 generates
+  the slug and put it to the changeset.
+
+    defmodule Post do
+      import Slugy, only: [slugify: 2]
+
+      schema "posts" do
+        field :title, :string
+        field :body, :text
+        field :published_at, :datetime
+        field :slug, :string
+      end
+
+      def changeset(post, attrs) do
+        post
+        |> cast(attrs, [:title, :body, :published_at])
+        |> slugify(:title)
+      end
+    end
+  """
+  import Ecto.Changeset
+
+  @doc ~S"""
+  Slugy.slugify/2 puts the slug generated on the given changeset and returns the changeset.
+
+  Returns the changeset itself if there are no changes to apply.
+
+  ## Examples
+
+      iex> slugify(changeset, :name)
+      %Ecto.Changeset{changes: %{slug: "a-slug-string-generated-from-name"}}
+
+      iex> slugify(changeset, :name)
+      %Ecto.Changeset{}
+  """
+  def slugify(changeset, key) when is_atom(key) do
+    if string = get_change(changeset, key) do
+      put_change(changeset, :slug, generate_slug(string))
+    else
+      changeset
+    end
+  end
+
   @doc """
   Returns a downcased dashed string.
 
   ## Examples
 
-      iex> Slugy.slugify("Hey ow lets go")
-      "hey-ow-lets-go"
+      iex> generate_slug("Vamo que vamo")
+      "vamo-que-vamo"
   """
-  def slugify(str) do
+  def generate_slug(str) do
     str
     |> String.normalize(:nfd)
     |> String.replace(~r/[^A-z\s\d]/u, "")
--- test/slugy_test.exs
+++ test/slugy_test.exs
@@ -1,9 +1,33 @@
 defmodule SlugyTest do
   use ExUnit.Case
-  doctest Slugy
 
-  test "slugy/1 downcase alphanumerics" do
-    assert Slugy.slugify("Hey ow lets go") == "hey-ow-lets-go"
-    assert Slugy.slugify("Olá, julia") == "ola-julia"
+  alias Ecto.Changeset
+
+  defmodule Post do
+    use Ecto.Schema
+
+    embedded_schema do
+      field(:title, :string)
+      field(:slug, :string)
+    end
+  end
+
+  describe "slugify/2" do
+    test "puts generated slug on changeset changes and returns changeset" do
+      changeset = Changeset.cast(%Post{}, %{title: "A new post"}, [:title])
+
+      assert %{changes: %{slug: "a-new-post"}} = Slugy.slugify(changeset, :title)
+    end
+
+    test "returns changeset when there is no changes to apply" do
+      changeset = Changeset.cast(%Post{}, %{}, [:title])
+
+      assert %{changes: %{}} = Slugy.slugify(changeset, :name)
+    end
+  end
+
+  describe "generate_slug/1" do
+    assert Slugy.generate_slug("Hey ow lets go") == "hey-ow-lets-go"
+    assert Slugy.generate_slug("Olá, julia") == "ola-julia"
   end
 end
