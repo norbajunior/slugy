64307fdce34782d6bdfbd5bea3e704abd81a1876
Cairo Noleto <caironoleto@gmail.com>
2018-10-20 09:46:40 -0300

    Update documentation with doctest.

--- test/slugy_test.exs
+++ test/slugy_test.exs
@@ -6,21 +6,39 @@ defmodule SlugyTest do
 
   defmodule Post do
     use Ecto.Schema
+    import Ecto.Changeset
+    import Slugy, only: [slugify: 2]
 
     embedded_schema do
       field(:title, :string)
       field(:slug, :string)
     end
+
+    def changeset(post, attrs) do
+      post
+      |> cast(attrs, [:title])
+      |> slugify(:title)
+    end
   end
 
   defmodule PostWithEmbeddedStruct do
     use Ecto.Schema
+    import Ecto.Changeset
+    import Slugy, only: [slugify: 2]
 
     embedded_schema do
-      field(:data, :map) # => %Post{data: %{title: "A title"}}
+      field(:data, :map) # => %PostWithEmbeddedStruct{data: %{title: "A title"}}
       field(:slug, :string)
     end
+
+    def changeset(post, attrs) do
+      post
+      |> cast(attrs, [:data])
+      |> slugify([:data, :title])
+    end
   end
+
+  doctest Slugy
 
   describe "slugify/2" do
     test "puts generated slug on changeset changes and returns changeset" do
