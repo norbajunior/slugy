d703f93056d63fe77583c4802763c974d54e26d0
Norberto Oliveira Junior <norbejunior@gmail.com>
2018-10-23 21:32:33 -0300

    bump version to 1.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Slugy.MixProject do
   def project do
     [
       app: :slugy,
-      version: "1.0.1",
+      version: "1.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: "A Phoenix library to generate slug for your schema fields",
